c5a0698d02084b653419cfa5431a211ae4476eee
smilingD13 <agarciagomez13@gmail.com>
2023-07-29 17:04:59 -0600

    validaciones

--- screens/CrearCuenta.js
+++ screens/CrearCuenta.js
@@ -1,29 +1,16 @@
-import { View, Text, Image, TextInput,ScrollView, TouchableOpacity, ActivityIndicator, Button } from 'react-native'
-import React, { useEffect, useLayoutEffect, useState } from 'react'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { View, Text, Image, TextInput,TouchableOpacity, Alert} from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { FontAwesome } from '@expo/vector-icons';
-import { Attractions, Avatar, Hotels, NotFound, Restaurants } from '../assets';
-import MenuContainer from '../components/MenuContainer';
-import ItemCardContainer from '../components/ItemCardContainer';
-import { getPlacesData } from '../api';
 
 
 
 const CrearCuenta = () => {
-
-  const [type, setType] = useState("restaurants");
-  const [isLoading, setIsLoading] = useState(false);
-  const [mainData, setMainData] = useState([]);
-  const [bl_lat, setBl_lat] = useState(null);
-  const [bl_lng, setBl_lng] = useState(null);
-  const [tr_lat, setTr_lat] = useState(null);
-  const [tr_lng, setTr_lng] = useState(null);
-
+  const [nombreUsuario, setNombreUsuario] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [correo, setCorreo] = useState('');
   const navigation = useNavigation();
-
-
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -32,28 +19,44 @@ const CrearCuenta = () => {
 
   }, []);
 
-  useEffect(() => {
-    setIsLoading(true);
-    getPlacesData(bl_lat, bl_lng, tr_lat, tr_lng, type).then((data) => {
-      setMainData(data);
-      setInterval(() => {
-        setIsLoading(false);
-      }, 2000);
-    });
-  }, [bl_lat, bl_lng, tr_lat, tr_lng, type]);
+  const validar = () =>{
+    if(!nombreUsuario || !contrasena || !correo || !nombre){
+      Alert.alert("Debe rellenar todos los campos.");
+      return true;
+    }
+    return false;
+  }
 
-
+  const guardar = () => {
+    if(!validar()){
+      Alert.alert("Usuario creado correctamente");
+      navigation.navigate("Discover")
+    }
+  }
+ 
   return (
     <SafeAreaView className="flex-1 bg-white flex-co justify-items-center">
       <Image source={require('../assets/pesonita2.gif')} className="mx-24" />
       <View className="flex bg-cyan-800 h-4/6 mx-4 rounded-xl py-1 px-2 shadow-lg ">
         <View className="flex-1 bg-cyan-700 h-96 m-4 rounded-xl py-1 px-4 shadow-lg shadow-black">
             <Text className="text-4xl mt-4 text-center text-white">Registrate</Text>
-            <TextInput className="bg-white m-2 rounded h-10 px-2" placeholder='Nombre*'></TextInput>
-            <TextInput className="bg-white m-2 rounded h-10 px-2" placeholder='Apellido*'></TextInput>
-            <TextInput className="bg-white m-2 rounded h-10 px-2" placeholder='Nombre de Usuario*'></TextInput>
-            <TextInput className="bg-white m-2 rounded h-10 px-2" placeholder='Contraseña*'></TextInput>
-            <TouchableOpacity className="bg-cyan-200 m-4 rounded h-10 p-3" onPress={() => navigation.navigate("Discover")}>
+            <TextInput className="bg-white m-2 rounded h-10 px-2" 
+            placeholder='Nombre*'
+            value={nombre}
+            onChangeText={setNombre}></TextInput>
+            <TextInput className="bg-white m-2 rounded h-10 px-2" 
+            placeholder='Correo*'
+            value={correo}
+            onChangeText={setCorreo}></TextInput>
+            <TextInput className="bg-white m-2 rounded h-10 px-2" 
+            placeholder='Nombre de Usuario*'
+            value={nombreUsuario}
+            onChangeText={setNombreUsuario}></TextInput>
+            <TextInput className="bg-white m-2 rounded h-10 px-2" 
+            placeholder='Contraseña*'
+            value={contrasena}
+            onChangeText={setContrasena}></TextInput>
+            <TouchableOpacity className="bg-cyan-200 m-4 rounded h-10 p-3" onPress={guardar}>
               <Text className="text-center font-extrabold" >COMENZAR</Text>
             </TouchableOpacity>
             <Text className="text-center my-2 text-white" onPress={() => navigation.navigate("Login")}>¿Ya tienes cuenta? Inicia Sesión</Text>
--- screens/Login.js
+++ screens/Login.js
@@ -1,29 +1,42 @@
-import { View, Text, Image, TextInput,ScrollView, TouchableOpacity, ActivityIndicator, Button } from 'react-native'
-import React, { useEffect, useLayoutEffect, useState } from 'react'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { View, Text, Image, TextInput,TouchableOpacity, Alert} from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { FontAwesome } from '@expo/vector-icons';
-import { Attractions, Avatar, Hotels, NotFound, Restaurants } from '../assets';
-import MenuContainer from '../components/MenuContainer';
-import ItemCardContainer from '../components/ItemCardContainer';
-import { getPlacesData } from '../api';
+
+
 
 
 
 const Login = () => {
 
-  const [type, setType] = useState("restaurants");
-  const [isLoading, setIsLoading] = useState(false);
-  const [mainData, setMainData] = useState([]);
-  const [bl_lat, setBl_lat] = useState(null);
-  const [bl_lng, setBl_lng] = useState(null);
-  const [tr_lat, setTr_lat] = useState(null);
-  const [tr_lng, setTr_lng] = useState(null);
+  const [nombreUsuario, setNombreUsuario] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  var contra = "P@ssw0rd";
+  var usuario = "elbicho";
+
 
   const navigation = useNavigation();
 
+  const validar = () =>{
+    var res = false;
+    if(!nombreUsuario || !contrasena){
+      Alert.alert("Debe rellenar ambos campos");
+      res = true;
+    }
 
+    if(nombreUsuario !== usuario || contrasena !== contra){
+      Alert.alert("Credenciales inválidas");
+      res = true;
+    }
+
+    return res;
+  }
+
+  const guardar = () =>{
+    if(!validar()){
+      navigation.navigate("Discover");
+    }
+  }
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -32,16 +45,7 @@ const Login = () => {
 
   }, []);
 
-  useEffect(() => {
-    setIsLoading(true);
-    getPlacesData(bl_lat, bl_lng, tr_lat, tr_lng, type).then((data) => {
-      setMainData(data);
-      setInterval(() => {
-        setIsLoading(false);
-      }, 2000);
-    });
-  }, [bl_lat, bl_lng, tr_lat, tr_lng, type]);
-
+  
 
   return (
     <SafeAreaView className="flex-1 bg-white">
@@ -49,10 +53,18 @@ const Login = () => {
       <View className="flex bg-cyan-800 h-96 mx-4 rounded-xl py-1 px-2 shadow-lg ">
         <View className="flex-1 bg-cyan-700 h-96 m-4 rounded-xl py-1 px-4 shadow-lg shadow-black">
             <Text className="text-4xl mt-4 text-center text-white">Bienvenido</Text>
-            <TextInput className="bg-white m-2 rounded h-10 px-2" placeholder='Usuario'></TextInput>
-            <TextInput className="bg-white m-2 rounded h-10 px-2" placeholder='Contraseña'></TextInput>
+            <TextInput className="bg-white m-2 rounded h-10 px-2" 
+            placeholder='Usuario'
+            value={nombreUsuario}
+            onChangeText={setNombreUsuario}
+            ></TextInput>
+            <TextInput className="bg-white m-2 rounded h-10 px-2" 
+            placeholder='Contraseña'
+            value={contrasena}
+            onChangeText={setContrasena}
+            ></TextInput>
             <TouchableOpacity className="bg-cyan-200 m-4 rounded h-10 p-3" onPress={() => navigation.navigate("Discover")}>
-              <Text className="text-center font-extrabold">LOGIN</Text>
+              <Text className="text-center font-extrabold" onPress={guardar}>LOGIN</Text>
             </TouchableOpacity>
             <Text className="text-center my-2 text-white" onPress={() => navigation.navigate("CrearCuenta")}>¿No tienes cuenta? Registrate</Text>
         </View>
